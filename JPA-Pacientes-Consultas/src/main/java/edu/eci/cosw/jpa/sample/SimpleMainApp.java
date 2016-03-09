521a868210e89f606c930c83a393d590109ef25c
Hector <hector.cadavid@gmail.com>
2016-03-09 08:00:38 -0500

    main program added

---
+++ JPA-Pacientes-Consultas/src/main/java/edu/eci/cosw/jpa/sample/SimpleMainApp.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 hcadavid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.cosw.jpa.sample;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class SimpleMainApp {
+   
+    public static void main(String a[]){
+        SessionFactory sf=getSessionFactory();
+        Session s=sf.openSession();
+        Transaction tx=s.beginTransaction();
+        
+        tx.commit();        
+    }
+
+    public static SessionFactory getSessionFactory() {
+        // loads configuration and mappings
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        ServiceRegistry serviceRegistry
+                = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        // builds a session factory from the service registry
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+
+}
